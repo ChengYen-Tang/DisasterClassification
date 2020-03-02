0571713ab82caa2f005143593c76fd8552256fc1
Kenneth Tang <kenneth@nssh.onmicrosoft.com>
2020-03-03 02:19:24 +0800

    pre train

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 from utils.image_processing import load_files
-from models.keras_model import setup_model
+from models.keras_model import setup_model, linear2
 from tensorflow import keras
 
 import numpy as np
@@ -13,10 +13,13 @@ images_shape = images.shape
 
 one_hot_label = one_hot(labels,4)
 
-model = setup_model((images_shape[1], images_shape[2], images_shape[3]), 4)
+model = setup_model((images_shape[1], images_shape[2], images_shape[3]))
+model.load_weights('./models/keras_model_weights.h5')
+model.add(linear2(4))
 model.compile(optimizer=keras.optimizers.Adadelta(),
              loss=keras.losses.SparseCategoricalCrossentropy(),
             metrics=['accuracy'])
 model.summary()
+model.save('./model.h5')
 
-history = model.fit(images, labels, batch_size=320, epochs=10000)
+history = model.fit(images, labels, batch_size=160, epochs=10000)
